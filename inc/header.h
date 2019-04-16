1f4bc6fb688b83b3b78e6c8bc1503dc8ce18f1ef
Alina Tarskya <djeanna@vo-i6.21-school.ru>
2019-04-16 15:54:14 +0300

    header

---
+++ inc/header.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djeanna <djeanna@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/16 15:48:49 by djeanna           #+#    #+#             */
+/*   Updated: 2019/04/16 15:53:51 by djeanna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif
