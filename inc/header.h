1e3c09e82c35bc707cdb93060d746c5fd8d0ff4e
Alina Tarskya <djeanna@vo-i6.21-school.ru>
2019-04-16 16:41:58 +0300

    header v.2

--- inc/header.h
+++ inc/header.h
@@ -6,7 +6,7 @@
 /*   By: djeanna <djeanna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 15:48:49 by djeanna           #+#    #+#             */
-/*   Updated: 2019/04/16 15:53:51 by djeanna          ###   ########.fr       */
+/*   Updated: 2019/04/16 16:41:40 by djeanna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,24 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+typedef struct	s_list
+{
+	char		**map;
+
+	t_list		*next;
+	t_list		*prev;
+}				t_list;
+
+t_list			*ft_list_new(char **map);
+t_list			*ft_list_pushback(t_list **beg, char **map);
+t_list			*ft_list_delone(t_list **beg, t_list *to_del);
+t_list			*ft_list_del(t_list **beg);
+
+int				get_next_line(const int fd, char **line);
+char			**ft_map_create(const int fd); //1
+t_list			*is_map_valid(const int fd); //2
+/* (char **) is pointer on the first '#'   */
+int				is_tetramino_valid(char **cur, char **prev);
 
 #endif
