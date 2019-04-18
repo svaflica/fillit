7d8cb178c20b63ebbb9cc80241f872a70ed21f4c
Asymmetrical Shari <ashari@vo-i4.21-school.ru>
2019-04-18 21:59:23 +0300

    ft_list_coordinate

---
+++ srcs/ft_find_coordinate.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_coordinate.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashari <ashari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/18 20:57:36 by ashari            #+#    #+#             */
+/*   Updated: 2019/04/18 21:57:32 by ashari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**		Return place, which we searched
+*/
+
+static	int		where_the_nearest(char *tetr, int *last, int *last_coord)
+{
+	int			nearest;
+	int			i;
+	int			step;
+
+	nearest = 4;
+	i = -1;
+	while (++i < 19)
+	{
+		step = -1;
+		while (++step < 4)
+		{
+			if ((i % 10) != 4 && (i % 10) != 9 && tetr[i] == '#')
+				(step < nearest) ? nearest = step : 0;
+			if (tetr[i] == '#')
+			{
+				*last_coord = step;
+				*last = i;
+			}
+			i++;
+		}
+	}
+	return (nearest);
+}
+
+static	int		give_me_your_size(char *tetr)
+{
+	int			h;
+	int			flag;
+	int			i;
+	int			step;
+
+	h = 0;
+	step = -1;
+	i = -1;
+	while (++i < 19)
+	{
+		flag = 0;
+		step = -1;
+		while (++step < 4)
+		{
+			if ((i % 10) != 4 && (i % 10) != 9 && tetr[i] == '#' && flag == 0)
+			{
+				flag = 1;
+				h++;
+			}
+			i++;
+		}
+	}
+	return (h);
+}
+
+int				ft_find_coordinate(char *tetr)
+{
+	int			nearest;
+	int			last_coord;
+	int			last;
+	int			h;
+	int			coord;
+
+	nearest = where_the_nearest(tetr, &last, &last_coord);
+	h = give_me_your_size(tetr);
+	coord = last - (last_coord - nearest) - (h - 1) * 5;
+	return (coord);
+}
