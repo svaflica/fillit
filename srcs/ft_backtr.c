b4eafe8d453891395edc183d6e11a45380b9f13d
Alina Tarskya <djeanna@at-p4.21-school.ru>
2019-04-20 16:13:26 +0300

    To_merge

--- srcs/ft_backtr.c
+++ srcs/ft_backtr.c
@@ -6,17 +6,45 @@
 /*   By: djeanna <djeanna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 18:30:37 by djeanna           #+#    #+#             */
-/*   Updated: 2019/04/20 14:32:35 by djeanna          ###   ########.fr       */
+/*   Updated: 2019/04/20 16:12:16 by djeanna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/header.h"
 
-static int		ft_insertion(t_lst *tetr, long long *map, int size, int row)
+static char		ft_helper(int w)
 {
-	char tmp;
+	char res;
 
-	while ()
+	res = 1;
+	while (--w)
+		res = (res << 1) | 1;
+	return (res);
+}
+
+static int		ft_insertion(t_lst *tetr, long long *map, int size,
+								int row, int shift)
+{
+	char		tmp;
+	long long	map_tmp[size];
+	int			h;
+	char		t;
+
+	tetr->x = size - shift - 1;
+	tetr->y = row;
+	t = ft_helper(tetr->w);
+	tmp = tetr->map;
+	h = tetr->h;
+	while (h)
+	{
+		map_tmp[row] = map[row] ^ ((tmp & t) << (size - shift + 1));
+		if (map_tmp[tmp] - map[tmp] == tmp)
+			return (0);
+		tmp = tmp >> tetr->w;
+		h--;
+	}
+	// copy from map_tmp to map
+	return (1);
 }
 
 static void		ft_free_from(t_lst *tetr, long long *map)
@@ -34,12 +62,12 @@ int				ft_try_to_solve(t_lst *tetr, long long *map, int size)
 	while (tetr)
 	{
 		if ((map[row] >> shift) & 1 == 0)
-			if (ft_insertion(tetr, map, size, row))
+			if (ft_insertion(tetr, map, size, row, shift))
 			{
 				ft_try_to_solve(tetr->next, map, size);
-				ft_free_from(tetr, map);
+				ft_free_from(tetr->next, map);
 			}
-			else if (row == size - 1 && tetr->w <= size - shift - 1 && // ?
+			else if (row == size - 1 && tetr->w <= size - shift - 1 &&
 						tetr->h >= size - row)
 				return (0);
 		if (--shift == -1 || tetr->w <= size - shift - 1)
@@ -48,5 +76,7 @@ int				ft_try_to_solve(t_lst *tetr, long long *map, int size)
 			row++;
 		}
 	}
+	while (--size)
+		printf("%d\n", map[size]);
 	return (1);
 }
